4a66b6810a3149e1c0c858867ee8cd7866f55ae6
Shahadat <sohan09014@gmail.com>
2016-09-10 18:38:35 +0600

    add some state machine test with System.out.println()

--- elasta-composer/src/main/java/elasta/composer/App.java
+++ elasta-composer/src/main/java/elasta/composer/App.java
@@ -8,6 +8,8 @@ import elasta.core.statemachine.StateMachine;
 import elasta.core.statemachine.StateTrigger;
 import io.vertx.core.Vertx;
 
+import static elasta.core.statemachine.StateEntry.on;
+
 /**
  * Created by Shahadat on 8/31/2016.
  */
@@ -17,16 +19,6 @@ public class App {
 
 //        final Vertx vertx = Vertx.vertx();
 
-        StateMachine.builder()
-                .when("start", StateEntry.on("ok", "respond"))
-                .handlers("start", StateMachine.exec(new FunctionUnchecked<Object, Promise<StateTrigger<String>>>() {
-                    @Override
-                    public Promise<StateTrigger<String>> apply(Object o) throws Throwable {
-                        System.out.println("ok gury");
-                        return Promises.just(StateMachine.<String>trigger("ok", "ok"));
-                    }
-                }, () -> null))
-                .setInitialState("start")
-                .build().start("start", "msg");
+
     }
 }
---
+++ elasta-core/src/main/java/elasta/core/StateMachineTest.java
@@ -0,0 +1,52 @@
+package elasta.core;
+
+import elasta.core.promise.impl.Promises;
+import elasta.core.statemachine.StateMachine;
+
+import static elasta.core.statemachine.StateEntry.on;
+
+/**
+ * Created by Jango on 9/10/2016.
+ */
+public class StateMachineTest {
+    public static void main(String[] args) {
+
+        test1();
+
+    }
+
+    private static void test1() {
+
+        StateMachine.builder()
+            .when("start",
+                on("ok", "respond"),
+                on("err", "errState"),
+                on("fuck", "fuckState"))
+            .handlers("errState", StateMachine.exec(o -> {
+                System.out.println("errState");
+                return Promises.just(StateMachine.exit("errState"));
+            }, null))
+            .handlers("fuckState", StateMachine.exec(null, () -> {
+                System.out.println("fuckState: end");
+                return Promises.empty();
+            }))
+            .handlers("respond", StateMachine.exec(t -> {
+                System.out.println("res");
+                return Promises.just(StateMachine.exit());
+            }, () -> {
+                System.out.println("res:end");
+                return Promises.empty();
+            }))
+            .handlers("start", StateMachine.exec(o -> {
+                System.out.println("ok gury");
+                return Promises.just(StateMachine.<String>trigger("fuck", "ok"));
+            }, () -> {
+                System.out.println("start: end");
+                return Promises.empty();
+            }))
+            .setInitialState("start")
+            .build().start("start", "msg")
+            .then(val -> System.out.println("got: " + val))
+            .err(e -> e.printStackTrace());
+    }
+}
